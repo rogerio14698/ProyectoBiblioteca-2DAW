c48e31702950eef10b0ccc23aff4f73f5be66a84
Practicas mglab <practicas@MGLAB.LOCAL>
2026-03-19 13:57:55 +0100

    implementado estilos css al login usuer y admin

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -10,7 +10,7 @@
         </div>
 
         @if ($errors->any())
-            <div class="alert alert-danger">
+            <div class="alertLogin alert-dangerLogin">
                 <ul>
                     @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
@@ -20,29 +20,34 @@
         @endif
 
         <div class="contenidoLogin">
-            <form action="{{ route('admin.login.procesar') }}" method="POST">
+            <form class="formLogin" action="{{ route('admin.login.procesar') }}" method="POST">
                 @csrf
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" value="{{ old('email') }}" required>
-                @error('email')
-                    <span class="error">{{ $message }}</span>
-                @enderror
+                <div class="labelLogin">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+                    @error('email')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
 
-                <label for="password">Contraseña:</label>
-                <input type="password" name="password" id="password" required>
-                @error('password')
-                    <span class="error">{{ $message }}</span>
-                @enderror
+                <div class="labelLogin">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" name="password" id="password" required>
+                    @error('password')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
 
-               <label for="recordar" class="recuerdame">
+               <div class="labelLogin">
+                <label for="recordar" class="recuerdame">
                     <input type="checkbox" name="recordar" id="recordar">
                     Recuérdame
                 </label>
+               </div>
 
                 <button type="submit" class="btn-base btn-verde">Iniciar Sesión como Admin</button>
             </form>
             <hr>
-            <p><a href="{{ route('usuario.login.mostrar') }}">¿Eres un usuario normal? Inicia sesión aquí</a></p>
         </div>
     </main>
 @endsection
--- resources/views/bibliotecaDAW/publicViews/login.blade.php
+++ resources/views/bibliotecaDAW/publicViews/login.blade.php
@@ -6,11 +6,10 @@
     <main class="contenedor login">
         <div class="loginHeader">
             <h1>Login - Usuario</h1>
-            <p>Bienvenido a la sección de login de la Biblioteca DAW.</p>
         </div>
         @if ($errors->any())
             <!--Tengo que definir estilos para las alertas -->
-            <div class="alert alert-danger">
+            <div class="alertLogin alert-dangerLogin">
                 <ul>
                     @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
@@ -20,24 +19,30 @@
         @endif
 
         <div class="contenidoLogin">
-            <form action="{{ route('usuario.login.procesar') }}" method="POST">
+            <form class="formLogin" action="{{ route('usuario.login.procesar') }}" method="POST">
                 @csrf
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" value="{{ old('email') }}" required>
-                @error('email')
-                    <span class="error">{{ $message }}</span>
-                @enderror
+                <div class="labelLogin">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+                    @error('email')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
 
-                <label for="password">Contraseña:</label>
-                <input type="password" name="password" id="password" required>
-                @error('password')
-                    <span class="error">{{ $message }}</span>
-                @enderror
+                <div class="labelLogin">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" name="password" id="password" required>
+                    @error('password')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+                </div>
 
-                <label for="recordar" class="recuerdame">
+                <div class="labelLogin">
+                    <label for="recordar" class="recuerdame">
                     <input type="checkbox" name="recordar" id="recordar">
                     Recuérdame
                 </label>
+                </div>
 
                 <button type="submit" class="btn-base btn-verde">Iniciar Sesión</button>
             </form>
